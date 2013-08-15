795b4ad9f2329d5376adc4de9cf97ec0a7524d6f
Don Mitchell <dmitchell@edx.org>
2013-08-15 09:25:13 -0400

    better equality testing primarily for unit tests

--- common/lib/xmodule/xmodule/x_module.py
+++ common/lib/xmodule/xmodule/x_module.py
@@ -654,26 +654,17 @@ class XModuleDescriptor(XModuleFields, HTMLSnippet, ResourceTemplates, XBlock):
         # We are not allowing editing of xblock tag and name fields at this time (for any component).
         return [XBlock.tags, XBlock.name]
 
+
     def get_explicitly_set_fields_by_scope(self, scope=Scope.content):
         """
         Get a dictionary of the fields for the given scope which are set explicitly on this xblock. (Including
         any set to None.)
         """
-        if scope == Scope.settings and hasattr(self, '_inherited_metadata'):
-            inherited_metadata = getattr(self, '_inherited_metadata')
-            result = {}
-            for field in self.fields.values():
-                if (field.scope == scope and
-                        self._field_data.has(self, field.name) and
-                        field.name not in inherited_metadata):
-                    result[field.name] = self._field_data.get(self, field.name)
-            return result
-        else:
-            result = {}
-            for field in self.fields.values():
-                if (field.scope == scope and self._field_data.has(self, field.name)):
-                    result[field.name] = self._field_data.get(self, field.name)
-            return result
+        result = {}
+        for field in self.fields.values():
+            if (field.scope == scope and self._field_data.has(self, field.name)):
+                result[field.name] = self._field_data.get(self, field.name)
+        return result
 
     @property
     def editable_metadata_fields(self):
@@ -682,8 +673,6 @@ class XModuleDescriptor(XModuleFields, HTMLSnippet, ResourceTemplates, XBlock):
 
         Can be limited by extending `non_editable_metadata_fields`.
         """
-        inherited_metadata = getattr(self, '_inherited_metadata', {})
-        inheritable_metadata = getattr(self, '_inheritable_metadata', {})
         metadata_fields = {}
 
         # Only use the fields from this class, not mixins
@@ -697,15 +686,20 @@ class XModuleDescriptor(XModuleFields, HTMLSnippet, ResourceTemplates, XBlock):
             if field.scope != Scope.settings or field in self.non_editable_metadata_fields:
                 continue
 
-            inheritable = False
-            value = getattr(self, field.name)
-            default_value = field.default
-            explicitly_set = self._field_data.has(self, field.name)
-            if field.name in inheritable_metadata:
-                inheritable = True
-                default_value = field.from_json(inheritable_metadata.get(field.name))
-                if field.name in inherited_metadata:
-                    explicitly_set = False
+            # gets the 'default_value', 'inheritable', and 'explicitly_set' attrs
+            try:
+                metadata_fields[field.name] = self.runtime.get_field_provenance(field)
+            except AttributeError:
+                # dhm: i believe only our crufty test systems can get here (in particular get_test_system)
+                metadata_fields[field.name] = {
+                    'default_value': field.default,
+                    'inheritable': False,
+                    'explicitly_set': self._field_data.has(self, field.name)
+                }
+            metadata_fields[field.name]['field_name'] = field.name
+            metadata_fields[field.name]['display_name'] = field.display_name
+            metadata_fields[field.name]['help'] = field.help
+            metadata_fields[field.name]['value'] = field.read_json(self)
 
             # We support the following editors:
             # 1. A select editor for fields with a list of possible values (includes Booleans).
@@ -719,6 +713,7 @@ class XModuleDescriptor(XModuleFields, HTMLSnippet, ResourceTemplates, XBlock):
                 if len(values) > 0:
                     editor_type = "Select"
                 for index, choice in enumerate(values):
+                    # why doesn't above deepcopy suffice?
                     json_choice = copy.deepcopy(choice)
                     if isinstance(json_choice, dict) and 'value' in json_choice:
                         json_choice['value'] = field.to_json(json_choice['value'])
@@ -731,22 +726,13 @@ class XModuleDescriptor(XModuleFields, HTMLSnippet, ResourceTemplates, XBlock):
                 editor_type = "Float"
             elif isinstance(field, List):
                 editor_type = "List"
-            metadata_fields[field.name] = {
-               'field_name': field.name,
-               'type': editor_type,
-               'display_name': field.display_name,
-               'value': field.to_json(value),
-               'options': [] if values is None else values,
-               'default_value': field.to_json(default_value),
-               'inheritable': inheritable,
-               'explicitly_set': explicitly_set,
-               'help': field.help,
-            }
+            metadata_fields[field.name]['type'] = editor_type
+            metadata_fields[field.name]['options'] = [] if values is None else values
 
         return metadata_fields
 
     # ~~~~~~~~~~~~~~~ XBlock API Wrappers ~~~~~~~~~~~~~~~~
-    def studio_view(self, context):
+    def studio_view(self, _context):
         """
         Return a fragment with the html from this XModuleDescriptor's editing view
 
@@ -759,6 +745,11 @@ class XModuleDescriptor(XModuleFields, HTMLSnippet, ResourceTemplates, XBlock):
 
 
 class DescriptorSystem(Runtime):
+    # where can I put these constants so that the kvs can use them w/o circular imports?
+    PROVENANCE_LOCAL = 'local'
+    PROVENANCE_DEFAULT = 'default'
+    PROVENANCE_INHERITED = 'inherited'
+
     def __init__(self, load_item, resources_fs, error_tracker, **kwargs):
         """
         load_item: Takes a Location and returns an XModuleDescriptor
@@ -805,6 +796,36 @@ class DescriptorSystem(Runtime):
     def get_block(self, block_id):
         """See documentation for `xblock.runtime:Runtime.get_block`"""
         return self.load_item(block_id)
+
+    def get_field_provenance(self, xblock, field):
+        """
+        For the given xblock, return a dict for the field's current state:
+        {
+            'default_value': what value will take effect if field is unset: either the field default or,
+            'inheritable': whether the default comes via inheritance rather than field default (boolean),
+            'explicitly_set': boolean for whether the current value is set v default/inherited,
+        }
+        :param xblock:
+        :param field:
+        """
+        # in runtime b/c runtime contains app-specific xblock behavior. Studio's the only app
+        # which needs this level of introspection right now. runtime also is 'allowed' to know
+        # about the kvs, dbmodel, etc.
+
+        # This method 'knows' about our 2 different kvs's and how they store data.
+        result = {}
+        result['explicitly_set'] = xblock._field_data.has(xblock, field.name)
+        try:
+            block_inherited = xblock.xblock_kvs.inherited_settings
+        except AttributeError:  # if inherited_settings doesn't exist on kvs
+            block_inherited = {}
+        if field.name in block_inherited:
+            result['default_value'] = block_inherited[field.name]
+            result['inheritable'] = True
+        else:
+            result['default_value'] = field.default
+            result['inheritable'] = False
+        return result
 
 
 class XMLParsingSystem(DescriptorSystem):
